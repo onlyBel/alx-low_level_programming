510b9175fcdfcb847ba44a08c03294f45f7e9492
onlyBel <belindawnjr@gmail.com>
2023-02-23 16:19:33 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
 #include "main.h"
 
 /**
- * main - prints _putchar and a new line
- *
+ * main - Entry point
  * Return: Always 0
  */
-
 int main(void)
-{
+{    
 	_putchar('_'); /*95 is asci for _*/
-	_putchar('p');
+	_putchar('p'); 
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('n');
-
+	_putchar('\n');
 	return (0);
 }
