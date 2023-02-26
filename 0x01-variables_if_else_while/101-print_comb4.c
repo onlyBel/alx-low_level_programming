4fe67babfa8941872b116dc698f4d93ec995cc7b
onlyBel <belindawnjr@gmail.com>
2023-02-25 23:25:06 -0800

    task 0

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 on succes
+ */
+int main(void)
+{
+	int s = 0, e = 7, s2 = 1, e2 = 8, s3 = 2, e3 = 9;
+
+	while (s <= e)
+	{
+		s3 = s2 + 1;
+		while (s3 <= e3)
+		{
+			putchar(s + '0');
+			putchar(s2 + '0');
+			putchar(s3 + '0');
+
+			if (s != e)
+			{
+				putchar('_');
+				putchar(' ');
+			}
+			s3++;
+		}
+		s2++;
+	}
+	s++;
+	
+	}
+	putchar('\n');
+
+	return (0);
+}
