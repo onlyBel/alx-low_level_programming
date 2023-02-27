1fc95e3521f2eccd4112f32ce074eb99c9f3fff9
onlyBel <belindawnjr@gmail.com>
2023-02-27 10:27:55 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - function declaration
+ * @str: string pointer
+ * Return: void
+ */
+
+void _puts(char *str);
+{
+	int index;
+
+	for (i = 0 ; str[i] != '\n'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+
