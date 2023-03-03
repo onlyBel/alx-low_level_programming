000059bb8528ab1aaef85d73207e5fad8132915f
onlyBel <belindawnjr@gmail.com>
2023-03-02 17:03:53 -0800

    task 0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strcpy(char *dest, char *src, int n);
+char *string_toupper(char *);
+char *rot13(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *leet(char *);
+int _putchar(char);
+char *cap_string(char *);
+void print_buffer(char *b, int size);
+
+
+
+#endif
