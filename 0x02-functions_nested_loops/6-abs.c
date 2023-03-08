065fe846ae41a7c4fa24e9bd53720060fbf6d4a8
onlyBel <belindawnjr@gmail.com>
2023-03-07 17:45:23 -0800

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -11,11 +11,11 @@
 int _abs(int i)
 {
 		if (i >= 0)
-				{
-							return (i);
-								}
-			else
-					{
-								return (i * -1);
-									}
+		{
+			return (i);
+		}
+		else
+		{
+			return (i * -1);
+		}
 }
