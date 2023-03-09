3e014f5876ca2f9cf66b66cbe5dbb08a54486753
onlyBel <belindawnjr@gmail.com>
2023-03-08 18:48:00 -0800

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,14 +6,14 @@
  *
  *Return: nothing on success.
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-if (*s)
-{
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
-}
-else
-	_putchar('\n');
+	if (*s)
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}	
+	else
+		_putchar('\n');
 }
