50ec084574f419694c3e9b1c287b175a746679b2
Belinda Wanjiru <belindawnjr@gmail.com>
2023-06-12 05:45:14 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,10 +10,13 @@ int _strlen_recursion(char *s)
 {
 	int longint = 0;
 
-	if(*s)
+	if (*s != '\0') 
 	{
 		longint++;
 		longint = _strlen_recursion(s + 1);
 	}
-	return(longint);
+	return
+		(1 + _strlen_recursion(s + 1);
+	else
+		return (0);
 }
