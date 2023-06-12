285213d38eb6043a3de0296e4079b2bf7bd5b385
Belinda Wanjiru <belindawnjr@gmail.com>
2023-06-12 05:35:25 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <conio.h>
+
 /**
  *_puts_recursion - a function that puts()
  *
@@ -8,7 +8,7 @@
  */
 void _puts_recursion(char *s)
 {
-	if(*s != '\0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		s++;
