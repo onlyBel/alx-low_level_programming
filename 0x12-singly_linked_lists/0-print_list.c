d2f4bd310e02241090442f2ca36c2ee28272b3a3
--add <belindawnjr@gmail.com>
2023-07-01 18:26:59 -0700

    printlist

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - print all elements of linked list
- *
- * @h: pointer
- *
- * Return: number of nodes printed
+ * print_list - check the code
+ * @h: name of the list
+ * Return: the number of nodes.
  */
 size_t print_list(const list_t *h)
 {
