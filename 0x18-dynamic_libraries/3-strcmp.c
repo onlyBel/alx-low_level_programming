df56e7b01110d955f932c642a9e3c4d30a7dead2
onlyBel <belindawnjr@gmail.com>
2023-10-20 17:36:49 -0700

    task 0

---
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - comprises two strings
+ * @s1: first string.
+ * @s2: second string.
+ * Return: 0 if s1 and s2 are equals.
+ * another number if not.
+ */
+
+int _strcmp (char *s1, char *s2)
+{
+	int i = 0, op = 0;
+
+	while (op == 0)
+	{
+		if ((*(s1 + 1) == '\0') && ((*s2 + 1) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+
+	return (op);
+} 
