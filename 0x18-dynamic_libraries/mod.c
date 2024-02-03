0a01d77659965db00e57c8ef64752c0b8aef7852
onlyBel <belindawnjr@gmail.com>
2024-02-02 19:16:08 -0800

    ✓

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mod - mode of two integers
+ * @a: first int
+ * @b: second int
+ * Return: final result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
